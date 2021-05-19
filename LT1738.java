65bca01b84bdd8724d145d20f2a81409026b2234
fu.qiang <fu.qiang@linecorp.com>
2021-05-19 11:00:51 +0800

    1738

---
+++ LT1738.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class LT1738 {
+
+    public int kthLargestValue(int[][] matrix, int k) {
+        int m = matrix.length, n = matrix[0].length;
+        int[] xors = new int[m * n];
+        xors[0] = matrix[0][0];
+        for (int j = 1; j < n; ++j) {
+            xors[j] = xors[j - 1] ^ matrix[0][j];
+        }
+        for (int i = 1; i < m; ++i) {
+            xors[i * n] = xors[(i - 1) * n] ^ matrix[i][0];
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                xors[i * n + j] = xors[(i - 1) * n + j] ^ xors[i * n + j - 1] ^ xors[(i - 1) * n + j - 1] ^ matrix[i][j];
+            }
+        }
+        Arrays.sort(xors);
+        return xors[xors.length - k];
+    }
+
+    public static void main(String[] args) {
+        LT1738 lt1738 = new LT1738();
+        System.out.println(lt1738.kthLargestValue(new int[][]{{5, 2}, {1, 6}}, 1)); // 7
+        System.out.println(lt1738.kthLargestValue(new int[][]{{5, 2}, {1, 6}}, 2)); // 5
+        System.out.println(lt1738.kthLargestValue(new int[][]{{5, 2}, {1, 6}}, 3)); // 4
+        System.out.println(lt1738.kthLargestValue(new int[][]{{5, 2}, {1, 6}}, 4)); // 0
+    }
+
+}
