048290bf672dc9bc38b180c6f3d38d68f2658adb
fu.qiang <fu.qiang@linecorp.com>
2021-07-22 11:25:34 +0800

    138

--- Test.java
+++ Test.java
@@ -1,33 +1,17 @@
-//public class Test {
-//
-//    public static void bag(int[] v, int[] w, int c) {
-//        int l = v.length;
-//        int[][] V = new int[l][c + 1];
-//        for (int i = 0; i < l; ++i) {
-//            V[i][0] = 0;
-//        }
-//        for (int i = 1; i < l; ++i) {
-//            for (int j = 0; j <= c; ++j) {
-//                if (j < w[i]) {
-//                    V[i][j] = V[i - 1][j];
-//                } else {
-//                    V[i][j] = Math.max(V[i - 1][j], V[i - 1][j - w[i]] + v[i]);
-//                }
-//            }
-//        }
-//        for (int i = 0; i < l; ++i) {
-//            for (int j = 0; j <= c; ++j) {
-//                System.out.print(V[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        int[] v = new int[]{3, 4, 5, 6};
-//        int[] w = new int[]{2, 3, 4, 5};
-//        int cap = 8;
-//        bag(v, w, 8);
-//    }
-//
-//}
+import java.util.PriorityQueue;
+
+public class Test {
+
+    public static void main(String[] args) {
+        PriorityQueue queue = new PriorityQueue();
+        queue.add(-3);
+        queue.add(-5);
+        queue.add(-1);
+        queue.add(-9);
+        queue.add(-4);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+
+}
