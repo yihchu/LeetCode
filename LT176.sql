0bf4c4f690529b9381213c9777f6ad748e461799
fu.qiang <fu.qiang@linecorp.com>
2021-09-07 10:01:45 +0800

    176

---
+++ LT176.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+
+select max(e2.Salary) as SecondHighestSalary from Employee e1 left join Employee e2 on e1.Salary > e2.Salary;
+
+# 官方答案用的limit offset
+
+/*
+编写一个SQL查询，获取Employee表中第二高的薪水（Salary）。
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+例如上述Employee表，SQL查询应该返回200作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回null。
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+*/
