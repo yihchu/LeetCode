93e8c767e599eac8bdf0065c6755a3eda0707d97
fu.qiang <fu.qiang@linecorp.com>
2021-09-22 10:22:23 +0800

    725

--- common/ListNode.java
+++ common/ListNode.java
@@ -3,5 +3,7 @@ package common;
 public class ListNode {
     public int val;
     public ListNode next;
+    ListNode() {}
     public ListNode(int x) { val = x; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
