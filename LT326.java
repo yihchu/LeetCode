fdfb89265ff6c2ab10dfc0d8d477f65503f6e17d
fu.qiang <fu.qiang@linecorp.com>
2021-09-23 09:45:44 +0800

    326

---
+++ LT326.java
@@ -0,0 +1,61 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LT326 {
+
+    private Set<Integer> sets = new HashSet<>() {{
+        int start = 1;
+        while (start > 0) {
+            add(start);
+            start *= 3;
+        }
+    }};
+
+    public boolean isPowerOfThree(int n) {
+        return sets.contains(n);
+    }
+
+//    // 官方答案，优秀
+//    public boolean isPowerOfThree(int n) {
+//        return n > 0 && 1162261467 % n == 0;
+//    }
+
+    public static void main(String[] args) {
+        LT326 lt326 = new LT326();
+        System.out.println(lt326.isPowerOfThree(27));
+        System.out.println(lt326.isPowerOfThree(0));
+        System.out.println(lt326.isPowerOfThree(9));
+        System.out.println(lt326.isPowerOfThree(45));
+    }
+
+}
+
+/**
+Link: https://leetcode-cn.com/problems/power-of-three/
+
+给定一个整数，写一个函数来判断它是否是3的幂次方。如果是，返回true；否则，返回false。
+整数n是3的幂次方需满足：存在整数x使得n == 3^x
+
+示例1：
+输入：n = 27
+输出：true
+
+示例2：
+输入：n = 0
+输出：false
+
+示例3：
+输入：n = 9
+输出：true
+
+示例4：
+输入：n = 45
+输出：false
+
+提示：
+-2^31 <= n <= 2^31 - 1
+
+进阶：
+你能不使用循环或者递归来完成本题吗？
+
+*/
