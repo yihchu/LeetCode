4375ffe32ab94c063ed8a6ee4898951679802429
fu.qiang <fu.qiang@linecorp.com>
2021-09-30 13:18:12 +0800

    delete 639 因为放假啦

--- common/TreeNode.java
+++ common/TreeNode.java
@@ -6,7 +6,13 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-    public TreeNode(int x) { val = x; }
+    public TreeNode() {}
+    public TreeNode(int x) { this.val = x; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 
     public static TreeNode fromArray(int[] array) {
         int len = array.length;
