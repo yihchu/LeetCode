3ffb84c86381161cd40ff443bcbd44a20819cc5c
fu.qiang <fu.qiang@linecorp.com>
2021-10-21 10:04:13 +0800

    453

--- LT453.java
+++ LT453.java
@@ -1,18 +1,24 @@
-//public class LT453 {
-//
-//    public int minMoves(int[] nums) {
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        LT453 lt453 = new LT453();
-////        System.out.println(lt453.minMoves(new int[] {1, 2, 3})); // 3
-////        System.out.println(lt453.minMoves(new int[] {1, 1, 1})); // 0
-//        System.out.println(lt453.minMoves(new int[] {5, 6, 8, 8, 5})); // 7
-//
-//    }
-//
-//}
+import java.util.Arrays;
+
+public class LT453 {
+
+    public int minMoves(int[] nums) {
+        int min = Arrays.stream(nums).min().getAsInt();
+        int result = 0, len = nums.length;
+        for (int i = 0; i < len; ++i) {
+            result += (nums[i] - min);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LT453 lt453 = new LT453();
+        System.out.println(lt453.minMoves(new int[] {1, 2, 3})); // 3
+        System.out.println(lt453.minMoves(new int[] {1, 1, 1})); // 0
+        System.out.println(lt453.minMoves(new int[] {5, 6, 8, 8, 5})); // 7
+    }
+
+}
 
 
 /**
