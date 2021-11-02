a5ab34619a7ba1560822b4090503b8698641deff
fu.qiang <fu.qiang@linecorp.com>
2021-11-02 11:11:25 +0800

    596

---
+++ In/House/Datebase/Q596.sql
@@ -0,0 +1,45 @@
+# Write your MySQL query statement below
+
+SELECT class FROM Courses group by class having count(distinct(student)) >= 5;
+
+/**
+有一个courses表，有: student(学生)和class(课程)。
+请列出所有超过或等于5名学生的课。
+
+例如，表：
++---------+------------+
+| student | class      |
++---------+------------+
+| A       | Math       |
+| B       | English    |
+| C       | Math       |
+| D       | Biology    |
+| E       | Math       |
+| F       | Computer   |
+| G       | Math       |
+| H       | Math       |
+| I       | Math       |
++---------+------------+
+
+应该输出:
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+
+提示：
+学生在每个课中不应被重复计算。
+
+Create table If Not Exists Courses (student varchar(255), class varchar(255))
+Truncate table Courses
+insert into Courses (student, class) values ('A', 'Math')
+insert into Courses (student, class) values ('B', 'English')
+insert into Courses (student, class) values ('C', 'Math')
+insert into Courses (student, class) values ('D', 'Biology')
+insert into Courses (student, class) values ('E', 'Math')
+insert into Courses (student, class) values ('F', 'Computer')
+insert into Courses (student, class) values ('G', 'Math')
+insert into Courses (student, class) values ('H', 'Math')
+insert into Courses (student, class) values ('I', 'Math')
+*/
