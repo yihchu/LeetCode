a9808fe90912c0b4974a1b1c1cc7b04614d028c2
fu.qiang <fu.qiang@linecorp.com>
2022-01-05 09:42:31 +0800

    913 1576

---
+++ In/House/Q1576/Solution.java
@@ -0,0 +1,67 @@
+package In.House.Q1576;
+
+public class Solution {
+
+    public String modifyString(String s) {
+        char[] chars = s.toCharArray();
+        char[] alters = new char[] {'a', 'b', 'c'};
+        int len = s.length();
+        for (int i = 0; i < len; ++i) {
+            if (chars[i] != '?') {
+                continue;
+            }
+            for (int j = 0; j < 3; ++j) {
+                if (i != 0 && chars[i - 1] == alters[j]) {
+                    continue;
+                }
+                if (i != len - 1 && chars[i + 1] == alters[j]) {
+                    continue;
+                }
+                chars[i] = alters[j];
+                break;
+            }
+        }
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        Solution slt = new Solution();
+        System.out.println(slt.modifyString("?zs")); // azs
+        System.out.println(slt.modifyString("ubv?w")); // "ubvaw"
+        System.out.println(slt.modifyString("j?qg??b")); // "jaqgacb"
+        System.out.println(slt.modifyString("??yw?ipkj?")); // "acywaipkja"
+    }
+
+}
+
+/**
+Link: https://leetcode-cn.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters/
+
+给你一个仅包含小写英文字母和'?'字符的字符串s，请你将所有的'?'转换为若干小写字母，使最终的字符串不包含任何连续重复的字符。
+注意：你不能修改非'?'字符。
+题目测试用例保证除'?'字符之外，不存在连续重复的字符。
+在完成所有转换（可能无需转换）后返回最终的字符串。如果有多个解决方案，请返回其中任何一个。可以证明，在给定的约束条件下，答案总是存在的。
+
+示例1：
+输入：s = "?zs"
+输出："azs"
+解释：该示例共有25种解决方案，从"azs" 到"yzs"都是符合题目要求的。只有"z"是无效的修改，因为字符串"zzs"中有连续重复的两个'z'。
+
+示例2：
+输入：s = "ubv?w"
+输出："ubvaw"
+解释：该示例共有24种解决方案，只有替换成"v"和"w"不符合题目要求。因为"ubvvw"和"ubvww"都包含连续重复的字符。
+
+示例3：
+输入：s = "j?qg??b"
+输出："jaqgacb"
+
+示例4：
+输入：s = "??yw?ipkj?"
+输出："acywaipkja"
+
+提示：
+1 <= s.length <= 100
+s仅包含小写英文字母和'?'字符
+
+*/
---
+++ In/House/Q913/Solution.java
@@ -0,0 +1,106 @@
+package In.House.Q913;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    private static final int MOUSE_WIN = 1;
+    private static final int CAT_WIN = 2;
+    private static final int DRAW = 0;
+    int n, threshold;
+    int[][] graph;
+    int[][][] dp;
+
+    public int catMouseGame(int[][] graph) {
+        this.n = graph.length;
+        this.threshold = n * 2;
+        this.graph = graph;
+        this.dp = new int[n][n][n * 2];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0;j < n; ++j) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return getResult(1, 2, 0);
+    }
+
+    private int getResult(int mouse, int cat, int turns) {
+        if (turns == threshold) {
+            return DRAW;
+        }
+        if (dp[mouse][cat][turns] < 0) {
+            if (mouse == 0) {
+                dp[mouse][cat][turns] = MOUSE_WIN;
+            } else if (mouse == cat) {
+                dp[mouse][cat][turns] = CAT_WIN;
+            } else {
+                getNextResult(mouse, cat, turns);
+            }
+        }
+        return dp[mouse][cat][turns];
+    }
+
+    private void getNextResult(int mouse, int cat, int turns) {
+        int curMove = turns % 2 == 0 ? mouse : cat;
+        int defaultResult = curMove == mouse ? CAT_WIN : MOUSE_WIN;
+        int result = defaultResult;
+        int[] nextNodes = graph[curMove];
+        for (int next: nextNodes) {
+            if (curMove == cat && next == 0) {
+                continue;
+            }
+            int nextMouse = curMove == mouse ? next : mouse;
+            int nextCat = curMove == cat ? next : cat;
+            int nextResult = getResult(nextMouse, nextCat, turns + 1);
+            if (nextResult != defaultResult) {
+                result = nextResult;
+                if (result != DRAW) {
+                    break;
+                }
+            }
+        }
+        dp[mouse][cat][turns] = result;
+    }
+
+    public static void main(String[] args) {
+        Solution slt = new Solution();
+        System.out.println(slt.catMouseGame(new int[][]{{2, 5}, {3}, {0, 4, 5}, {1, 4, 5}, {2, 3}, {0, 2, 3}}));
+        System.out.println(slt.catMouseGame(new int[][]{{1, 3}, {0}, {3}, {0, 2}}));
+    }
+
+}
+
+/**
+Link: https://leetcode-cn.com/problems/cat-and-mouse/
+
+两位玩家分别扮演猫和老鼠，在一张无向图上进行游戏，两人轮流行动。
+图的形式是：graph[a]是一个列表，由满足ab是图中的一条边的所有节点b组成。
+老鼠从节点1开始，第一个出发；猫从节点2开始，第二个出发。在节点0处有一个洞。
+在每个玩家的行动中，他们必须沿着图中与所在当前位置连通的一条边移动。例如，如果老鼠在节点1，那么它必须移动到graph[1]中的任一节点。
+此外，猫无法移动到洞中（节点0）。
+然后，游戏在出现以下三种情形之一时结束：
+如果猫和老鼠出现在同一个节点，猫获胜。
+如果老鼠到达洞中，老鼠获胜。
+如果某一位置重复出现（即，玩家的位置和移动顺序都与上一次行动相同），游戏平局。
+给你一张图graph，并假设两位玩家都都以最佳状态参与游戏：
+如果老鼠获胜，则返回1；
+如果猫获胜，则返回2；
+如果平局，则返回0。
+
+示例1(图1.jpg)：
+输入：graph = [[2, 5], [3], [0, 4, 5], [1, 4, 5], [2, 3], [0, 2, 3]]
+输出：0
+
+示例2(图2.jpg)：
+输入：graph = [[1, 3], [0], [3], [0, 2]]
+输出：1
+
+提示：
+3 <= graph.length <= 50
+1<= graph[i].length < graph.length
+0 <= graph[i][j] < graph.length
+graph[i][j] != i
+graph[i]互不相同
+猫和老鼠在游戏中总是移动
+
+*/
