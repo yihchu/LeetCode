9a8329ca19eaedac3ed11ab306d2306fa2cc1f89
fu.qiang <fu.qiang@linecorp.com>
2022-01-17 11:01:45 +0800

    1220

--- In/House/Q1220/Solution.java
+++ In/House/Q1220/Solution.java
@@ -1,33 +1,22 @@
 package In.House.Q1220;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
 
+    // 哇，刷Hard，一下就想到了正解，真开心
     public int countVowelPermutation(int n) {
         final int THRESHOLD = 1000000007;
-        Map<Character, Integer> map = new HashMap<>() {{
-            put('a', 1);
-            put('e', 1);
-            put('i', 1);
-            put('o', 1);
-            put('u', 1);
-        }};
+        int[] arr = new int[] {1, 1, 1, 1, 1};
         for (int i = 1; i < n; ++i) {
-            int countA = (map.get('e') + map.get('i')) % THRESHOLD + map.get('u');
-            int countE = map.get('a') + map.get('i');
-            int countI = map.get('e') + map.get('o');
-            int countO = map.get('i');
-            int countU = map.get('i') + map.get('o');
-            map.put('a', countA % THRESHOLD);
-            map.put('e', countE % THRESHOLD);
-            map.put('i', countI % THRESHOLD);
-            map.put('o', countO % THRESHOLD);
-            map.put('u', countU % THRESHOLD);
+            int[] tmp = new int[5];
+            tmp[0] = ((arr[1] + arr[2]) % THRESHOLD + arr[4]) % THRESHOLD;
+            tmp[1] = (arr[0] + arr[2]) % THRESHOLD;
+            tmp[2] = (arr[1] + arr[3]) % THRESHOLD;
+            tmp[3] = arr[2];
+            tmp[4] = (arr[2] + arr[3]) % THRESHOLD;
+            arr = tmp;
         }
         int sum = 0;
-        for (Integer s: map.values()) {
+        for (int s: arr) {
             sum += s;
             sum %= THRESHOLD;
         }
