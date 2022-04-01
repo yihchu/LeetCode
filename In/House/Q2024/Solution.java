1215684cd814ff636307b172a88446847935985d
fu.qiang <fu.qiang@linecorp.com>
2022-04-01 08:31:53 +0800

    2024

--- In/House/Q2024/Solution.java
+++ In/House/Q2024/Solution.java
@@ -1,5 +1,8 @@
 package In.House.Q2024;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     public int maxConsecutiveAnswers(String answerKey, int k) {
@@ -8,24 +11,25 @@ public class Solution {
     }
 
     private int find(char[] chars, char target, int k) {
-        int start = 0, end = 0, len = chars.length;
-        int idx = 0, count = 0;
-        while (chars[idx] != target) {
-            ++idx;
-        }
-        start = idx;
-        count = 1;
-        while (count < k) {
-            ++idx;
-            if (chars[idx] != target) {
-                continue;
+        int len = chars.length;
+        List<Integer> list = new ArrayList<>();
+        list.add(-1);
+        for (int i = 0; i < len; ++i) {
+            if (chars[i] == target) {
+                list.add(i);
             }
-            ++idx;
         }
-        end = idx;
-        int result = end - start + 1;
-        while (chars[idx] )
-        while (end < )
+        list.add(len);
+        int count = list.size();
+        if (count - 2 <= k) {
+            return len;
+        }
+        int max = -1;
+        for (int i = k + 1; i < count; ++i) {
+            int tmp = (list.get(i) - 1) - (list.get(i - k - 1) + 1) + 1;
+            max = Math.max(tmp, max);
+        }
+        return max;
     }
 
     public static void main(String[] args) {
