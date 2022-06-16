0c38ee801197f0155f8e0cdea529ca1945d853c9
fu.qiang <fu.qiang@linecorp.com>
2022-06-16 10:37:32 +0800

    532

--- In/House/Q532/Solution.java
+++ In/House/Q532/Solution.java
@@ -1,27 +1,32 @@
 package In.House.Q532;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Solution {
 
     public int findPairs(int[] nums, int k) {
         Arrays.sort(nums);
         int count = 0, len = nums.length;
-        Set<Integer> set = new HashSet<>();
-        for (int j = len - 1; j > 0; --j) {
-            for (int i = 0; i < j; ++i) {
-                if (nums[j] - nums[i] < k) {
+        for (int j = len - 1; j > 0; ) {
+            if (nums[j] - nums[0] < k) {
+                break;
+            }
+            int l = 0, r = j - 1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                int diff = nums[j] - nums[mid];
+                if (diff > k) {
+                    l = mid + 1;
+                } else if (diff < k) {
+                    r = mid - 1;
+                } else {
+                    ++count;
                     break;
                 }
-                if (nums[j] - nums[i] > k) {
-                    continue;
-                }
-                if (!set.contains(nums[i])) {
-                    set.add(nums[i]);
-                    ++count;
-                }
+            }
+            --j;
+            while (j > 0 && nums[j] == nums[j + 1]) {
+                --j;
             }
         }
         return count;
@@ -32,6 +37,8 @@ public class Solution {
         System.out.println(slt.findPairs(new int[] {3, 1, 4, 1, 5}, 2)); // 2
         System.out.println(slt.findPairs(new int[] {1, 2, 3, 4, 5}, 1)); // 4
         System.out.println(slt.findPairs(new int[] {1, 3, 1, 5, 4}, 0)); // 1
+        System.out.println(slt.findPairs(new int[] {1, 2, 4, 4, 3, 3, 0, 9, 2, 3}, 3)); // 2
+        System.out.println(slt.findPairs(new int[] {1, 1, 1, 2, 2}, 0)); // 2
     }
 
 }
