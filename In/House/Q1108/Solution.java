c178055f762c96bcc3d4f687ada9679330bbdabb
fu.qiang <fu.qiang@linecorp.com>
2022-06-21 10:14:21 +0800

    1108

--- In/House/Q1108/Solution.java
+++ In/House/Q1108/Solution.java
@@ -2,8 +2,23 @@ package In.House.Q1108;
 
 public class Solution {
 
+//    public String defangIPaddr(String address) {
+//        return address.replaceAll("\\.", "[\\.]");
+//    }
+
     public String defangIPaddr(String address) {
-        return address.replaceAll("\\.", "[\\.]");
+        int len = address.length(), idx = 0;
+        char[] chars = new char[len + 6], origin = address.toCharArray();
+        for (char c: origin) {
+            if (c != '.') {
+                chars[idx ++] = c;
+                continue;
+            }
+            chars[idx ++] = '[';
+            chars[idx ++] = c;
+            chars[idx ++] = ']';
+        }
+        return new String(chars);
     }
 
     public static void main(String[] args) {
